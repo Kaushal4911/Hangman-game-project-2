1a933eedcbecb0ccf1e439eae09f19a488c1668e
Kaushal Bhoir <112846503+Kaushal4911@users.noreply.github.com>
2024-06-27 17:58:24 +0530

    Add files via upload

---
+++ hangman game/scripts/word list.js
@@ -0,0 +1,83 @@
+const wordList = [
+    {
+        word: "Guitar",
+        hint: "A musical instrument with strings."
+    },
+    {
+        word: "Oxygen",
+        hint: "A colorless, odorless gas essential for life."
+    },
+    {
+        word: "Mountain",
+        hint: "A large natural elevation of the Earth's surface."
+    },
+    {
+        word: "Painting",
+        hint: "An art form using colors on a surface to create images or expression."
+    },
+    {
+        word: "Astronomy",
+        hint: "The scientific study of celestial objects and phenomena."
+    },
+    {
+        word: "Football",
+        hint: "A popular sport played with a spherical ball."
+    },
+    {
+        word: "Chocolate",
+        hint: "A sweet treat made from cocoa beans."
+    },
+    {
+        word: "Butterfly",
+        hint: "An insect with colorful wings and a slender body."
+    },
+    {
+        word: "History",
+        hint: "The study of past events and human civilization."
+    },
+    {
+        word: "Pizza",
+        hint: "A savory dish consisting of a round, flattened base with toppings."
+    },
+    {
+        word: "Jazz",
+        hint: "A genre of music characterized by improvisation and syncopation."
+    },
+    {
+        word: "Camera",
+        hint: "A device used to capture and record images or videos."
+    },
+    {
+        word: "Diamond",
+        hint: "A precious gemstone known for its brilliance and hardness."
+    },
+    {
+        word: "Advanture",
+        hint: "An execiting or daring experience."
+    },
+    {
+        word: "Science",
+        hint: "The systematic study of the structure and behavior of the physical. "
+    },
+    {
+        word: "Bicycle",
+        hint: "A human-powered vehicle with two wheels."
+    },
+    {
+        word: "Sunset",
+        hint: "The daily disappearance of the sun below the horizon."
+    },
+    {
+        word: "Coffee",
+        hint: "A popular caffeinated beverage made from roasted coffee beans."
+    },
+    {
+        word: "Dance",
+        hint: "A rhythmatic movement of the body often performed to music."
+    },
+    {
+        word: "Kaushal",
+        hint: "my self."
+    },
+
+]
